772cfa191237e3dc2b50e699208e44b11446c6e6
Hazem Hamdy7 <hazemhamdy1215@gmail.com>
2024-04-29 11:55:58 -0700

    add weather service

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -1,8 +1,30 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
+
+import '../model/weather_model.dart';
 
 class WeatherSeavice {
   final Dio dio;
+  final String baseUrl = 'http://api.weatherapi.com/v1';
+  final String apiKey = 'f65075417336485388273336241603';
   WeatherSeavice(this.dio);
 
-  
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio
+          .get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
+
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errMessage =
+          e.response?.data['error']['message'] ?? ' oops there was an error';
+      throw Exception(errMessage);
+    } catch (e) {
+      log(e.toString());
+      throw Exception(' oops there was an error');
+    }
+  }
 }
